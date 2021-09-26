0e82a767b82d431960a0bb231ba0f21990d06ad4
Dpost-George <65887520+Dpost-George@users.noreply.github.com>
2021-09-26 05:04:04 +0200

    Hashing Ok have to complete verifyblock

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,16 +28,60 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+ for (let line of poem) {
+	 createBlock(line)
+ }
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+// Create a function called createBlock()
+ 		/* Block content:
+ 			`index`
+			`prevHash`
+			`data`
+			`timestamp`
+ 			`hash`
+		 */
+ function createBlock(_data) {
+
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data: _data,
+		timestamp: Date.now(),
+	}
+	block.hash = blockHash(block)
+	Blockchain.blocks.push(block)
+	console.log(block)
+	return block
+ }
 
 
+//console.log(`Blockchain is valid: ${verifyBlock(Blockchain)}`);*/
+/* function verifyBlock(bl) {
+    if (bl.data == null) return false;
+    if (bl.index === 0) {
+        if (bl.hash !== "000000") return false;
+    }
+    else {
+        if (!bl.prevHash) return false;
+        if (!(
+            typeof bl.index === "number" &&
+            Number.isInteger(bl.index) &&
+            bl.index > 0
+        )) {
+            return false;
+        }
+        if (bl.hash !== blockHash(bl)) return false;
+    }
+
+    return true;
+}*/ 
 // **********************************
 
 function blockHash(bl) {
+	
+	let block = JSON.stringify(bl)
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		/*`${bl.index};${bl.prevHash};${bl.data};{$bl.timestamp};`*/ block
 	).digest("hex");
 }
